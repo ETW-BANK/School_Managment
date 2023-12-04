38bd8950a49e67c5a8b7b1715c01810117d425ee
dinozoff1981 <tensaeg@yahoo.com>
2023-12-04 12:12:19 +0100

    change method name

--- SchoolManagment/SchoolManagment/Action/Actions.cs
+++ SchoolManagment/SchoolManagment/Action/Actions.cs
@@ -50,73 +50,7 @@ namespace SchoolManagment.Action
                 reader.Close();
             }
         }
-        //public static void AddStudent( SqlConnection conn)
-
-        //{
-        //    {
-        //        using (SqlCommand cmd =new SqlCommand())
-        //        {
-
-
-        //            // Insert data into the Students table
-        //        Console.WriteLine("\n\t\t\tInserting data into the Students table.");
-        //        Console.WriteLine("\t\t\t=========================================\n\n");
-        //        Console.WriteLine("Enter First Name");
-        //        string Fname = Console.ReadLine().ToUpper();
-        //        Console.WriteLine("Enter Last Name");
-        //        string Lname = Console.ReadLine().ToUpper();
-        //        Console.WriteLine("Enter Student Gender");
-        //        string gender = Console.ReadLine().ToUpper();
-        //        Console.WriteLine("Enter Course Name");
-        //        string course = Console.ReadLine().ToUpper();
-        //        Console.WriteLine("Enter Age");
-        //        int age = Convert.ToInt32(Console.ReadLine());
-        //        Console.WriteLine("Enter Class Name");
-        //       string clasname = Console.ReadLine().ToUpper();
-
-        //       cmd.CommandType = CommandType.Text;
-        //       cmd.Connection = conn;   
-
-        //        cmd.CommandText = "insert into Student(FirstName,LastName,Gender,Age)OUTPUT INSERTED.Student_ID values(@Fname,@Lname,@gender,@age)";
-        //        cmd.Parameters.AddWithValue("@Fname", Fname);
-        //        cmd.Parameters.AddWithValue("@Lname", Lname);
-        //        cmd.Parameters.AddWithValue("@gender", gender);
-        //        cmd.Parameters.AddWithValue("@age", age);
-
-        //        cmd.CommandText = "insert into Course(CourseTitel)OUTPUT INSERTED.Course_ID values(@coursetitle)";
-        //        cmd.Parameters.AddWithValue("@coursetitle", course);
-
-        //       cmd.CommandText = "insert into Class(ClassName)OUTPUT INSERTED.Class_ID values(@classname)";
-        //       cmd.Parameters.AddWithValue("@classname", clasname);
-
-        //        int studentId = Convert.ToInt32(cmd.ExecuteScalar());
-        //        int courseId = Convert.ToInt32(cmd.ExecuteScalar());
-        //        int classId=Convert.ToInt32(cmd.ExecuteScalar());
-
-
-
-        //        cmd.CommandText = "insert into Relation(StudentID,CourseID,ClassID) values(@studentId,@courseId,@classId)";
-        //        cmd.Parameters.Clear(); // Clear previous parameters
-        //        cmd.Parameters.AddWithValue("@studentId", studentId);
-        //        cmd.Parameters.AddWithValue("@courseId", courseId);
-        //        cmd.Parameters.AddWithValue("@classId", classId);
-
-        //        int rowsAffected = cmd.ExecuteNonQuery();
-
-        //        if (rowsAffected > 0)
-        //        {
-        //            Console.WriteLine("\t\t\tData inserted into the Students table.\n");
-        //        }
-        //        else
-        //        {
-        //            Console.WriteLine("\t\t\tFailed to insert data into the Students table.");
-        //        }
-        //            Thread.Sleep(1000);
-        //            Console.Clear();
-        //    }
-        //}
-
-        //}
+     
 
 
         public static void AddStudent(SqlConnection conn)
